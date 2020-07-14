05f198337f49e47fab54bc7b7edd8dae0a8540fc
sefu <zephaniahsefuoduori@gmail.com>
2020-07-14 18:57:00 +0300

    Add card to display the notification on screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: AppBar(
+            backgroundColor: Color(0xfff79631),
+          )),
+      body: Container(
+        child: Stack(
+          children: <Widget>[_itemOne(context), _itemTwo(context)],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+Widget _itemOne(BuildContext context) => Container(
+      child: Card(elevation: 10, color: const Color(0xffffffff)),
+      height: 126.0,
+      width: MediaQuery.of(context).size.width,
+    );
+
+Widget _itemTwo(BuildContext context) => Container(
+      child: Card(elevation: 10, color: const Color(0xffffffff)),
+      height: 122.0,
+      width: MediaQuery.of(context).size.width,
+    );
